75de5a24646c96dd3337f8afffbf0adaa5201537
yangaound2017 <31610399+yangaound2017@users.noreply.github.com>
2017-09-19 12:41:34 +0800

    Update trace.py

--- logutil/trace.py
+++ logutil/trace.py
@@ -30,14 +30,13 @@ def handle_exception(logger, throws=False):
 
 class Trace:
     @staticmethod
-    def caller_stack(pointer=2):
-        print inspect.stack()[pointer]
+    def _caller_stack(pointer=2):
         return inspect.stack()[pointer]
 
     @staticmethod
     def cls(caller_stack=None):
         try:
-            stack = caller_stack or Trace.caller_stack()
+            stack = caller_stack or Trace._caller_stack()
             cls = stack[0].f_locals['self'].__class__
             return cls.__module__ + '.' + cls.__name__
         except KeyError:
@@ -46,36 +45,36 @@ class Trace:
     @staticmethod
     def method(caller_stack=None):
         try:
-            stack = caller_stack or Trace.caller_stack()
+            stack = caller_stack or Trace._caller_stack()
             cls = stack[0].f_locals['self'].__class__
             return cls.__module__ + '.' + cls.__name__ + '.' + stack[3]
         except KeyError:
             return AssertionError('outside of context, no class object')
 
     @staticmethod
-    def moduel(caller_stack=None):
-        stack = caller_stack or Trace.caller_stack()
+    def module(caller_stack=None):
+        stack = caller_stack or Trace._caller_stack()
         return stack[0].f_globals.get('__name__')
 
     @staticmethod
     def func(caller_stack=None):
-        stack = caller_stack or Trace.caller_stack()
+        stack = caller_stack or Trace._caller_stack()
         if stack[3] == '<module>':
             return AssertionError('outside of context, no function object')
         return stack[0].f_globals.get('__name__') + '.' + stack[3]
 
     @staticmethod
     def line(caller_stack=None):
-        stack = caller_stack or Trace.caller_stack()
+        stack = caller_stack or Trace._caller_stack()
         ns = stack[0].f_globals.get('__name__')
-        return "{ns}:{line}".format(ns=ns, line=stack[2])
+        return "{ns}.py:{line}".format(ns=ns, line=stack[2])
 
 
 class Traceable(object):
-    __clsname = 'Namable'
+    __clsname = 'Traceable'
 
     def this(self):
-        return u"{ns}.{obj}:{line}".format(ns=self.__class__.__module__, obj=self.__class__.__name__, line=Trace.caller_stack()[2])
+        return u"{ns}.{obj}".format(ns=self.__class__.__module__, obj=self.__class__.__name__, )
 
     @classmethod
     def base(cls, level=0):
@@ -85,5 +84,5 @@ class Traceable(object):
 
         attr_name = '_' + cls.__name__ + '__clsname'
         if hasattr(cls, attr_name):
-            return u"{ns}.{obj}:{line}".format(ns=cls.__module__, obj=getattr(cls, attr_name), line=Trace.caller_stack()[2])
+            return u"{ns}.{obj}".format(ns=cls.__module__, obj=getattr(cls, attr_name), )
         return cls.__base__.base(level + 1)
